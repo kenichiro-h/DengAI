ca3e7a9e2b1e710860036ac8879078268137b1c2
ken <honda@hotaruco.com>
2018-04-08 09:01:28 +0900

    comment out  making image

--- R/read_data.R
+++ R/read_data.R
@@ -15,44 +15,45 @@ submission_sample <- read.table("Desktop/DengAI/data/submission_format.csv",sep 
 train <- left_join(label, train_features) 
 train_long <- train %>% gather(key, value, -c(1:3, 5))
 
-#aggregate
-# Histogram ----
-hi <- train_long %>%
-      ggplot(aes(value, fill = city)) +
-      geom_histogram(bins = 30) +
-      facet_wrap(~ key, scales = "free") +
-      scale_fill_viridis(discrete = TRUE)
-ggsave("Desktop/DengAI/image/test.png", plot = hi)
 
-# Line graph ----
-g_line <- train_long %>%
-            ggplot(aes(week_start_date, value, color = city, group = city)) +
-            geom_line() +
-            facet_wrap(~ key, scales = "free") +
-            scale_color_viridis(discrete = TRUE)
-ggsave("Desktop/DengAI/image/test2.png", plot = g_line)
-
-# Total cases ----
-case <- train_long %>%
-          filter(key == "total_cases") %>% 
-          ggplot(aes(week_start_date, value, color = city, group = city)) +
-          geom_line() +
-          scale_color_viridis(discrete = TRUE)
-ggsave("Desktop/DengAI/image/test3.png", plot = case)
-
-# Total cases per weekofyear ----
-case_per_week <- train_long %>% 
-                    filter(key == "total_cases") %>% 
-                    ggplot(aes(weekofyear, value, color = city, group = year)) +
-                    geom_line() +
-                    scale_color_viridis(discrete = TRUE) +
-                    facet_wrap(~ city)
-ggsave("Desktop/DengAI/image/test4.png", plot = case_per_week)
-
-# Distribution of events ----
-event <- train_long %>%
-            filter(key == "total_cases") %>% 
-            ggplot(aes(log2(value + 1), fill = city)) +
-            geom_histogram(bins = 60) +
-            scale_fill_viridis(discrete = TRUE)
-ggsave("Desktop/DengAI/image/test5.png", plot = event)
+# #aggregate
+# # Histogram ----
+# hi <- train_long %>%
+#       ggplot(aes(value, fill = city)) +
+#       geom_histogram(bins = 30) +
+#       facet_wrap(~ key, scales = "free") +
+#       scale_fill_viridis(discrete = TRUE)
+# ggsave("Desktop/DengAI/image/test.png", plot = hi)
+# 
+# # Line graph ----
+# g_line <- train_long %>%
+#             ggplot(aes(week_start_date, value, color = city, group = city)) +
+#             geom_line() +
+#             facet_wrap(~ key, scales = "free") +
+#             scale_color_viridis(discrete = TRUE)
+# ggsave("Desktop/DengAI/image/test2.png", plot = g_line)
+# 
+# # Total cases ----
+# case <- train_long %>%
+#           filter(key == "total_cases") %>% 
+#           ggplot(aes(week_start_date, value, color = city, group = city)) +
+#           geom_line() +
+#           scale_color_viridis(discrete = TRUE)
+# ggsave("Desktop/DengAI/image/test3.png", plot = case)
+# 
+# # Total cases per weekofyear ----
+# case_per_week <- train_long %>% 
+#                     filter(key == "total_cases") %>% 
+#                     ggplot(aes(weekofyear, value, color = city, group = year)) +
+#                     geom_line() +
+#                     scale_color_viridis(discrete = TRUE) +
+#                     facet_wrap(~ city)
+# ggsave("Desktop/DengAI/image/test4.png", plot = case_per_week)
+# 
+# # Distribution of events ----
+# event <- train_long %>%
+#             filter(key == "total_cases") %>% 
+#             ggplot(aes(log2(value + 1), fill = city)) +
+#             geom_histogram(bins = 60) +
+#             scale_fill_viridis(discrete = TRUE)
+# ggsave("Desktop/DengAI/image/test5.png", plot = event)
